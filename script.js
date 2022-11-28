62c5fa365f8e99354901b0cecd3a10b97e304078
taras <tarik2002sta@gmail/com>
2022-11-28 12:26:48 +0200

    firct commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const howMuch = Number(prompt('Введіть годину щоб перевести її у секунди'));
+
+const moreSeconds = 3600;
+
+const sum = howMuch * moreSeconds;
+alert('Hours--> 🕙︎('+howMuch+ ') Seconds--> 🕙︎('+ sum+')');
